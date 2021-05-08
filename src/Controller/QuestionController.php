2ee6cc3c50bd641fa0f64cc0b4b5dcdff08d9be5
Daniel Backes <danielbackes@protonmail.com>
2021-05-08 17:49:05 -0400

    feat: create markdown server

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,8 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
-use Symfony\Contracts\Cache\CacheInterface;
+use App\Service\MarkdownHelper;
 
 class QuestionController extends AbstractController
 {
@@ -29,7 +28,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -39,9 +38,7 @@ class QuestionController extends AbstractController
 
         $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
 
-        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($markdownParser, $questionText) {
-            return $markdownParser->transformMarkdown($questionText);
-        });
+        $parsedQuestionText = $markdownHelper->parse($questionText);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
